0c570fb45fce14be94e8a19edfb73dc6e40932e1
unknown <Tonystark0316-opokuacheampong236@gmail.com>
2026-04-09 01:25:09 -0100

    my assignment

---
+++ anthonyacheampong/lecture/main.py
@@ -0,0 +1,90 @@
+# #second task
+# name = input ("what is your name : ")
+
+# print("Hello " + name)
+
+# #third task
+# #capitalizing of input
+# name = input ("Enter your name : ")
+# print(f"hello {name.upper()}" )
+
+
+# #users full name
+
+# def input_name():
+#   first_name = input("Enter your first name : ")
+#   sur_name = input("Enter surname : ")
+#   return( first_name + sur_name)
+
+# input_name()
+
+# def take_name():
+#   first_name = input("Enter your first name : ")
+#   sur_name = input("Enter last name : ")
+#   return f"your fullname {first_name} + {sur_name}"
+
+# take_name()
+
+# #fifth task
+# day = input(" Enter the day : ")
+
+# #checking the day function
+
+# def checking_day():
+
+#   if day == "Monday":
+#       return"Monday"
+  
+#   elif day == "Tuesday":
+#      return"Tuesday"
+  
+#   elif day == "Wednessday":
+#      return"Wednessday"
+  
+#   elif day == "Thursday":
+#      return"Thursday"
+  
+#   elif day == "Friday":
+#      return"Friday"
+  
+#   elif day == "Saturday":
+#      return"Saturday"
+  
+#   elif day == "Sunday":
+#      return"Sunday"
+  
+#   else:
+#      return"invalid day"
+  
+#   print(checking_day())
+
+
+#calculator
+def calculator():
+ 
+  num1 = int(input("Enter first number : "))
+  num2 = int(input("Enter second number : "))
+  rule = input("Enter rule : ")
+
+  if rule == "add":
+   return num1 + num2
+
+  elif rule == "sub":
+   return num1 - num2
+
+  elif rule == "mul":
+   return num1 * num2
+
+  elif rule == "div":
+   return num1 / num2 
+
+  else:
+   return "invalid"
+#print(calculator())
+
+def whileloop():
+    n = 0
+    while n < 0:
+      print("meow")
+    n+= 1
+whileloop()
